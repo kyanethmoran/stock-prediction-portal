dc5ebc0129bcd8cfcd22126f63771f0ca832c07f
moran <sharon.ky.moran@gmail.com>
2025-07-26 00:53:34 -0400

    Style header

--- frontend-React/src/components/Header.jsx
+++ frontend-React/src/components/Header.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
 
 const Header = () => {
   return (
-    <>
-      <h2>Header</h2>
-    </>
+    <nav className="navbar navbar-expand-lg bg-dark py-3 shadow-sm">
+      <div className="container d-flex justify-content-between align-items-center">
+        <a
+          href="/"
+          className="navbar-brand d-flex align-items-center text-light"
+        >
+          <img
+            src={logo}
+            alt="Logo"
+            width="40"
+            height="40"
+            className="me-2 rounded-circle shadow"
+          />
+          <span className="fs-4 fw-bold text-info">Stock+</span>
+        </a>
+
+        <div>
+          <a className="btn btn-outline-light me-2 hover-shadow">Login</a>
+          <a className="btn btn-info text-white">Sign Up</a>
+        </div>
+      </div>
+    </nav>
   );
 };
 
