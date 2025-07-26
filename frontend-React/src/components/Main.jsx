b425c66a91eb6305219efa7160ee15537c1a19cd
moran <sharon.ky.moran@gmail.com>
2025-07-26 01:13:51 -0400

    main information completed

--- frontend-React/src/components/Main.jsx
+++ frontend-React/src/components/Main.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import machineLearning from "../assets/images/machineLearning.jpg";
 
 const Main = () => {
   return (
     <>
-      <h2>Main</h2>
+      <section className="bg-light py-5">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-6 mb-4 mb-md-0">
+              <h1 className="display-5 fw-bold text-dark">
+                Smarter Stock Analysis with Machine Learning
+              </h1>
+              <p className="lead text-muted mt-3">
+                Stock+ uses powerful machine learning to uncover patterns,
+                predict market movements, and give you the edge in real-time
+                decision making.
+              </p>
+              <a
+                href="/signup"
+                className="btn btn-info btn-lg mt-3 text-white shadow"
+              >
+                Get Started
+              </a>
+            </div>
+
+            <div className="col-md-6 text-center">
+              <img
+                src={machineLearning}
+                className="bg-secondary rounded shadow-sm d-flex justify-content-center align-items-center"
+                style={{ height: "300px", width: "100%" }}
+              />
+            </div>
+            <div>
+              <p className="text-center text-primary small shadow mt-2 px-3">
+                <strong>Disclaimer:</strong> This website is a portfolio project
+                created for demonstration purposes only. It is not a real
+                product and should <u>not</u> be used to make actual trading or
+                investment decisions. No data, predictions, or insights provided
+                here are financial advice.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
