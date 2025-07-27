fe0ce690baf2fe6687ec783ca71543a17311bfe7
moran <sharon.ky.moran@gmail.com>
2025-07-27 01:29:25 -0400

    reviser user view to accounts

--- backend-DjangoRestFramework/accounts/views.py
+++ backend-DjangoRestFramework/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.ibjects.all()
+    serializer_class = UserSerializer
+    # anyone should be able to create a user
+    permission_class = [AllowAny]
