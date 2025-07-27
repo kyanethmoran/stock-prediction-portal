7dace6788ece8c9e815b5aa44d12d5c1c707cf78
moran <sharon.ky.moran@gmail.com>
2025-07-27 01:44:19 -0400

    give password a minimun length requirement

--- backend-DjangoRestFramework/accounts/serializers.py
+++ backend-DjangoRestFramework/accounts/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     # so that the password can be read
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    password = serializers.CharField(write_only=True, min_length=4, style={'input_type': 'password'})
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
