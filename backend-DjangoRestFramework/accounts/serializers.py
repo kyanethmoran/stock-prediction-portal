f7f49e5980fe7a4140b9a9d467f21cebde210544
moran <sharon.ky.moran@gmail.com>
2025-07-27 01:43:05 -0400

    give password style input type password to hide password in browsable api

--- backend-DjangoRestFramework/accounts/serializers.py
+++ backend-DjangoRestFramework/accounts/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     # so that the password can be read
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
