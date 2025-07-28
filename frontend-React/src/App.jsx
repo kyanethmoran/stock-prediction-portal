62e818e646bb8fbc5c0bc385795b1dbd4caa7bdf
moran <sharon.ky.moran@gmail.com>
2025-07-27 22:50:25 -0400

    ensure header and footer show up on every page

--- frontend-React/src/App.jsx
+++ frontend-React/src/App.jsx
@@ -1,16 +1,20 @@
 import "./assets/css/style.css";
-import Main from "./components/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
