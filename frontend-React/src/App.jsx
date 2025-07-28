4ab46fb64720ba2b43c7cab5a28ec95c52d8eadd
moran <sharon.ky.moran@gmail.com>
2025-07-27 23:17:18 -0400

    update Routes for login

--- frontend-React/src/App.jsx
+++ frontend-React/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
