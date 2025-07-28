c00686267a733fc8a70b022ed674c3b7a248efbe
moran <sharon.ky.moran@gmail.com>
2025-07-27 22:47:40 -0400

    spelling

--- frontend-React/src/App.jsx
+++ frontend-React/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
