1ca098de64453673fc14fa85f34afe9a1a6dfef8
moran <sharon.ky.moran@gmail.com>
2025-07-30 00:55:43 -0400

    add dashboard protected view to views

--- backend-DjangoRestFramework/accounts/views.py
+++ backend-DjangoRestFramework/accounts/views.py
@@ -3,6 +3,9 @@ from .serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
@@ -10,3 +13,14 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     # anyone should be able to create a user
     permission_class = [AllowAny]
+
+# protected view
+class DashboardView(APIView):
+    #only authorized users with valid access token can access this 
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        response = {
+            'status': "Request was allowed"
+        }
+        return Response(response)
