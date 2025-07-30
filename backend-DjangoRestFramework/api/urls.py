e2cdd487e49ce38f56059249d9f41f2fe92cd89d
moran <sharon.ky.moran@gmail.com>
2025-07-30 00:52:59 -0400

    comment added

--- backend-DjangoRestFramework/api/urls.py
+++ backend-DjangoRestFramework/api/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    # protected view path
     path('dashboard/', UserViews.DashboardView.as_view()),
 ]
