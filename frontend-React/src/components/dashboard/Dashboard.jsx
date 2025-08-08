6e5c223dae9f4dee8a19f23f7eb3be81d890f40b
moran <sharon.ky.moran@gmail.com>
2025-08-07 23:53:27 -0400

    spelling error fix

--- frontend-React/src/components/dashboard/Dashboard.jsx
+++ frontend-React/src/components/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import axiosInstance from "../../axiosInstance";
 
 const Dashboard = () => {
   const accessToken = localStorage.getItem("accessToken");
