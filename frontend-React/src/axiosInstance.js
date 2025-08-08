b0cd671fa4d1756de9341a11041eb77bdba9a2ca
moran <sharon.ky.moran@gmail.com>
2025-08-07 23:53:10 -0400

    spelling error fix

--- frontend-React/src/axiosInstance.js
+++ frontend-React/src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BACKEND_BASE_API;
-const axiousInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: baseURL,
 });
 
-export default axiousInstance;
+export default axiosInstance;
