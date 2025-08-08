71a473e7e621159314d6d959e3d1ea7c42d8ff6f
moran <sharon.ky.moran@gmail.com>
2025-08-08 00:24:43 -0400

    refresh token handled by response interceptor

--- frontend-React/src/axiosInstance.js
+++ frontend-React/src/axiosInstance.js
@@ -19,4 +19,34 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+//Response Interceptor (for refresh token)
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  //handle failed response
+  async function (error) {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest.retry) {
+      originalRequest.retry = true;
+      const refreshToken = localStorage.getItem("refreshToken");
+      try {
+        const response = await axiosInstance.post("/token/refresh/", {
+          refresh: refreshToken,
+        });
+        localStorage.setItem("accessToken", response.data.access);
+        originalRequest.headers[
+          "Authorization"
+        ] = `Bearer ${response.data.access}`;
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
