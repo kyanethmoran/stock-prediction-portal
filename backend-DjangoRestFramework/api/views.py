28a0b945c4ee8659a5ae7be25eafeb73c5212c43
moran <sharon.ky.moran@gmail.com>
2025-08-18 15:48:42 -0400

    if no matching data in the dataframe to the stock ticker respond with error 404

--- backend-DjangoRestFramework/api/views.py
+++ backend-DjangoRestFramework/api/views.py
@@ -4,6 +4,13 @@ from .serializers import StockPredictionSerializer
 from rest_framework import status
 from rest_framework.response import Response
 
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
 # Create your views here.
 # APIView over generics because we want more control over the custom calculations
 class StockPredictionAPIView(APIView):
@@ -11,4 +18,16 @@ class StockPredictionAPIView(APIView):
         serializer = StockPredictionSerializer(data = request.data)
         if serializer.is_valid():
             ticker = serializer.validated_data['ticker']
+
+            #fetch the data from yfinance (same code as in jupyter notebook)
+            now = datetime.now()
+            start = datetime(now.year-10, now.month, now.day)
+            end = now
+            df = yf.download(ticker, start, end)
+            print(df)
+
+            if df.empty:
+                return Response({'error': "No data found for the given ticker.",
+                                 'status': status.HTTP_404_NOT_FOUND})
+
             return Response({'status': 'success', 'ticker': ticker})
