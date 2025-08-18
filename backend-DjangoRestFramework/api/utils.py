d5391baa9f2b7c3ffab6a967389b3c76cb0fb38d
moran <sharon.ky.moran@gmail.com>
2025-08-18 17:10:26 -0400

    remove white space padding from plot

--- backend-DjangoRestFramework/api/utils.py
+++ backend-DjangoRestFramework/api/utils.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 def save_plot(plot_img_path):
     image_path = os.path.join(settings.MEDIA_ROOT, plot_img_path)
-    plt.savefig(image_path)
+    plt.savefig(image_path, bbox_inches='tight')
     plt.close()
     image_url = settings.MEDIA_URL + plot_img_path
     return image_url
