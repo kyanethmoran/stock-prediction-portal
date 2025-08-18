425225992554d6fde1ecb3a43b627b5e88151e8a
moran <sharon.ky.moran@gmail.com>
2025-08-18 16:20:35 -0400

    add the media folder for the charts to the urls

--- backend-DjangoRestFramework/stock_prediction_main/urls.py
+++ backend-DjangoRestFramework/stock_prediction_main/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Base API Endpoint
+    # Base API Endpoint:
+    # Route all /api/v1/ requests to api/urls (API v1 entry point)
+    # + serve user-uploaded media files in development from MEDIA_ROOT at MEDIA_URL
     path('api/v1/', include('api.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
